9cc1bce85a8bcc21798f6922c8a4b0be1420cf54
danh <danhdanh@gmail.com>
2017-09-28 10:59:10 +0700

    git truoc khi luu du lieu

--- app/src/main/java/com/example/danhnc/maptalk/CommentFragment.java
+++ app/src/main/java/com/example/danhnc/maptalk/CommentFragment.java
@@ -1,7 +1,5 @@
 package com.example.danhnc.maptalk;
 
-import android.content.Context;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
@@ -11,32 +9,35 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class PositionFragment extends Fragment {
-    TextView tv1,tv2, tvtoado;
+/**
+ * Created by danhnc on 9/26/2017.
+ */
+
+public class CommentFragment extends Fragment {
+    TextView tv1;
+    TextView tv2;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_position, container, false);
-        Toast.makeText(getContext(), "day la position fragment", Toast.LENGTH_SHORT).show();
+        View view = inflater.inflate(R.layout.fragment_comment, container, false);
+        Toast.makeText(getContext(), "day la comment fragment", Toast.LENGTH_SHORT).show();
         tv1 = (TextView) view.findViewById(R.id.textView1);
         tv2 = view.findViewById(R.id.textView2);
-        tv1.setEnabled(false);
-        tv2.setEnabled(true);
-        tvtoado = view.findViewById(R.id.textView3);
-        //String toado= getArguments().getString("toa do");
-        //tvtoado.setText(toado);
+        tv1.setEnabled(true);
+        tv2.setEnabled(false);
 
-        tv2.setOnClickListener(new View.OnClickListener(){
+        tv1.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view) {
-                Fragment newCommentFragment = new CommentFragment();
+                Fragment newCommentFragment = new PositionFragment();
                 FragmentTransaction transaction = getFragmentManager().beginTransaction();
 
                 transaction.replace(R.id.flPosition,newCommentFragment);
                 transaction.addToBackStack(null);
 
-                transaction.commit();}
-            });
+                transaction.commit();
+            }
+        });
         return view;
     }
 }
